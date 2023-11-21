d07d36f191b463d4b74221f1425815f686f19494
Shuvo <shuvoorba28@gmail.com>
2023-11-21 21:57:54 +0600

    protection layer added to the add new item for admin

--- index.js
+++ index.js
@@ -108,7 +108,7 @@ async function run() {
       res.send(result);
     })
 
-    app.patch('/users/admin/:id', async(req, res) => {
+    app.patch('/users/admin/:id', verifyToken, verifyAdmin, async(req, res) => {
       const id = req.params.id; 
       const filter = {_id: new ObjectId(id)}
       const updatedDoc = {
@@ -120,7 +120,7 @@ async function run() {
       res.send(result); 
     })
 
-    app.delete('/users/:id', async(req, res) => {
+    app.delete('/users/:id', verifyToken, verifyAdmin, async(req, res) => {
       const id = req.params.id; 
       const query = { _id: new ObjectId(id)}
       const result = await userCollection.deleteOne(query); 
