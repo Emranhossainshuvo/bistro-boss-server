76322bdb827083500c6740636f1002528ea993c6
Shuvo <shuvoorba28@gmail.com>
2023-11-21 07:18:08 -0800

    JWT installed and access token secret created

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const app = express();
+const jwt = require('jsonwebtoken')
 const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
