e171b4acd366b007a88a29f05ce4c247e78401b0
Shuvo <shuvoorba28@gmail.com>
2023-11-22 15:17:07 +0600

    api created to delete a menu item from the database

--- index.js
+++ index.js
@@ -71,10 +71,6 @@ async function run() {
       res.send({token})
     })
 
-
-
-   
-
     app.get('/users/admin/:email', verifyToken, async(req, res) => {
       const email = req.params.email; 
       if(email !== req.decoded.email){
@@ -134,6 +130,20 @@ async function run() {
         const result = await menuCollection.find().toArray(); 
         res.send(result); 
     })
+
+    app.post('/menu', verifyToken, verifyAdmin, async(req, res) => {
+      const item = req.body; 
+      const result = await menuCollection.insertOne(item); 
+      res.send(result)
+    })
+
+    // app.delete('/menu/:id', verifyToken, verifyAdmin, async(req, res) => {
+    //   const id = req.params.id; 
+    //   const query = {_id: new ObjectId(id)}; 
+    //   const result = await menuCollection.deleteOne(query); 
+    //   res.send(result); 
+    // })
+
     app.get('/reviews', async(req, res) => {
         const result = await reviewsCollection.find().toArray(); 
         res.send(result); 
